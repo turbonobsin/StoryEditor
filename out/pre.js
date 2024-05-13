d3dfa6267c667c0abfb2caf43097b78dedbf75bf
turbonobsin <87375599+turbonobsin@users.noreply.github.com>
2024-05-12 22:26:19 -0400

    compiled front end

--- out/pre.js
+++ out/pre.js
@@ -1,5 +1,6 @@
 // let serverURL = "http://localhost:3000";
-let serverURL = "https://next-overly-urchin.ngrok-free.app";
+// let serverURL = "https://next-overly-urchin.ngrok-free.app";
+let serverURL = "https://claebcode.top:3001";
 const AID = "__SE-"; // app id
 let page = 0;
 let isOffline = true;
